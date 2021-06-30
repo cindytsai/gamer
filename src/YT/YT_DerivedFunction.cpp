6163a465b706edc882aebe1c770bef3c0be92191
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-06-30 23:10:48 +0800

    Fixed bug in blurry def in grid_dimensions that yt_getGridInfo_Dimensions get.

--- src/YT/YT_DerivedFunction.cpp
+++ src/YT/YT_DerivedFunction.cpp
@@ -10,6 +10,7 @@
 //
 // Note        :  1. This function's pointer will be passed into libyt.
 //                2. The argument should be declared like this, in order to match the libyt API.
+//                3. yt_getGridInfo_Dimensions() gets the grid_dimensions[0][1][2] in [x][y][z] coordinate.
 //
 // Parameter   :  GID            : Grid GID
 //                Converted_MagX : Store the converted field data here.
@@ -19,19 +20,19 @@
 void MagX_DerivedFunc(long gid, double *Converted_MagX){
     // Get the dimension of the grid, and the data array pointer of the grid
     int Dimensions[3];
-    void *DataRaw;
+    yt_data DataRaw;
     yt_getGridInfo_Dimensions( gid, &Dimensions );
     yt_getGridInfo_FieldData( gid, "MagX", &DataRaw );
 
     // Cast the DataRaw
-    real *Data = (real *) DataRaw;
+    real *Data = (real *) DataRaw.data_ptr;
 
     // Compute converted field via data. [z, y, x] direction.
-    for (int k=0; k<Dimensions[0]; k++){
+    for (int k=0; k<Dimensions[2]; k++){
         for (int j=0; j<Dimensions[1]; j++){
-            for (int i=0; i<Dimensions[2]; i++){
-                int idx_Bx = IDX321_BX(i, j, k, Dimensions[2], Dimensions[1]);
-                int idx_cc = i + j * Dimensions[2] + k * Dimensions[2] * Dimensions[1];
+            for (int i=0; i<Dimensions[0]; i++){
+                int idx_Bx = IDX321_BX(i, j, k, Dimensions[0], Dimensions[1]);
+                int idx_cc = i + j * Dimensions[0] + k * Dimensions[0] * Dimensions[1];
                 Converted_MagX[idx_cc] = (double) 0.5 * ( Data[ idx_Bx ] + Data[ idx_Bx + 1 ] );
             }
         }
@@ -41,20 +42,20 @@ void MagX_DerivedFunc(long gid, double *Converted_MagX){
 void MagY_DerivedFunc(long gid, double *Converted_MagY){
     // Get the dimension of the grid, and the data array pointer of the grid
     int Dimensions[3];
-    void *DataRaw;
+    yt_data DataRaw;
     yt_getGridInfo_Dimensions( gid, &Dimensions );
     yt_getGridInfo_FieldData( gid, "MagY", &DataRaw );
 
     // Cast the DataRaw
-    real *Data = (real *) DataRaw;
+    real *Data = (real *) DataRaw.data_ptr;
 
     // Compute converted field via data. [z, y, x] direction.
-    for (int k=0; k<Dimensions[0]; k++){
+    for (int k=0; k<Dimensions[2]; k++){
         for (int j=0; j<Dimensions[1]; j++){
-            for (int i=0; i<Dimensions[2]; i++){
-                int idx_By = IDX321_BY(i, j, k, Dimensions[2], Dimensions[1]);
-                int idx_cc = i + j * Dimensions[2] + k * Dimensions[2] * Dimensions[1];
-                Converted_MagY[idx_cc] = (double) 0.5 * ( Data[ idx_By ] + Data[ idx_By + Dimensions[2] ] );
+            for (int i=0; i<Dimensions[0]; i++){
+                int idx_By = IDX321_BY(i, j, k, Dimensions[0], Dimensions[1]);
+                int idx_cc = i + j * Dimensions[0] + k * Dimensions[0] * Dimensions[1];
+                Converted_MagY[idx_cc] = (double) 0.5 * ( Data[ idx_By ] + Data[ idx_By + Dimensions[0] ] );
             }
         }
     }
@@ -63,20 +64,20 @@ void MagY_DerivedFunc(long gid, double *Converted_MagY){
 void MagZ_DerivedFunc(long gid, double *Converted_MagZ){
     // Get the dimension of the grid, and the data array pointer of the grid
     int Dimensions[3];
-    void *DataRaw;
+    yt_data DataRaw;
     yt_getGridInfo_Dimensions( gid, &Dimensions );
     yt_getGridInfo_FieldData( gid, "MagZ", &DataRaw );
 
     // Cast the DataRaw
-    real *Data = (real *) DataRaw;
+    real *Data = (real *) DataRaw.data_ptr;
 
     // Compute converted field via data. [z, y, x] direction.
-    for (int k=0; k<Dimensions[0]; k++){
+    for (int k=0; k<Dimensions[2]; k++){
         for (int j=0; j<Dimensions[1]; j++){
-            for (int i=0; i<Dimensions[2]; i++){
-                int idx_Bz = IDX321_BZ(i, j, k, Dimensions[2], Dimensions[1]);
-                int idx_cc = i + j * Dimensions[2] + k * Dimensions[2] * Dimensions[1];
-                Converted_MagZ[idx_cc] = (double) 0.5 * ( Data[ idx_Bz ] + Data[ idx_Bz + Dimensions[2] * Dimensions[1] ] );
+            for (int i=0; i<Dimensions[0]; i++){
+                int idx_Bz = IDX321_BZ(i, j, k, Dimensions[0], Dimensions[1]);
+                int idx_cc = i + j * Dimensions[0] + k * Dimensions[0] * Dimensions[1];
+                Converted_MagZ[idx_cc] = (double) 0.5 * ( Data[ idx_Bz ] + Data[ idx_Bz + Dimensions[0] * Dimensions[1] ] );
             }
         }
     }
@@ -101,20 +102,20 @@ void MagZ_DerivedFunc(long gid, double *Converted_MagZ){
 void VelocityX_DerivedFunc(long gid, double *VelocityX){
     // Get the dimension of the grid, and the data array pointer of the grid
     int Dimensions[3];
-    void *DataRaw1, *DataRaw2;
+    yt_data DataRaw1, DataRaw2;
     yt_getGridInfo_Dimensions( gid, &Dimensions );
     yt_getGridInfo_FieldData( gid, "MomX", &DataRaw1 );
     yt_getGridInfo_FieldData( gid, "Dens", &DataRaw2 );
 
     // Cast the DataRaw1/2
-    real *MomX = (real *) DataRaw1;
-    real *Dens = (real *) DataRaw2;
+    real *MomX = (real *) DataRaw1.data_ptr;
+    real *Dens = (real *) DataRaw2.data_ptr;
 
     // Compute converted field via data. [z, y, x] direction.
-    for (int k=0; k<Dimensions[0]; k++){
+    for (int k=0; k<Dimensions[2]; k++){
         for (int j=0; j<Dimensions[1]; j++){
-            for (int i=0; i<Dimensions[2]; i++){
-                int idx_cc = i + j * Dimensions[2] + k * Dimensions[2] * Dimensions[1];
+            for (int i=0; i<Dimensions[0]; i++){
+                int idx_cc = i + j * Dimensions[0] + k * Dimensions[0] * Dimensions[1];
                 VelocityX[idx_cc] = (double) MomX[idx_cc] / Dens[idx_cc];
             }
         }
